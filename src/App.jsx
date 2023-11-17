0eb9f2b29638d6c2dc3151d6cd0867cf3a1d136c
jrfm <moesch.jonas@protonmail.com>
2023-11-17 12:19:46 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { MathJaxContext } from "better-react-mathjax";
+
+// defining config for MathJax
+// see react-better MathJax documentation
+const config = {
+  "fast-preview": {
+    disabled: true,
+  },
+  tex2jax: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+  },
+  messageStyle: "none",
+};
+
+function App() {
+  return (
+    <MathJaxContext
+      version={2}
+      config={config}
+      onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
+    ></MathJaxContext>
+  );
+}
+
+export default App;
